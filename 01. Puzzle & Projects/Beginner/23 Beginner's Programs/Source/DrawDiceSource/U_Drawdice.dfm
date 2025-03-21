586d3cb737b7a7c6ca9818a1b533fbebb1e9e081
DELMadang <delmadang95@gmail.com>
2025-03-21 09:27:48 +0900

    주사위 그리기 수정

--- 01. Puzzle & Projects/Beginner/23 Beginner's Programs/Source/DrawDiceSource/U_Drawdice.dfm
+++ 01. Puzzle & Projects/Beginner/23 Beginner's Programs/Source/DrawDiceSource/U_Drawdice.dfm
@@ -1,30 +1,27 @@
 object Form1: TForm1
   Left = 192
   Top = 133
-  Width = 696
-  Height = 206
-  Caption = 'Form1'
+  Caption = 'Draw Random Dice'
+  ClientHeight = 158
+  ClientWidth = 264
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'MS Sans Serif'
   Font.Style = []
-  OldCreateOrder = False
   Position = poScreenCenter
-  OnActivate = FormActivate
-  PixelsPerInch = 96
   TextHeight = 13
   object Image1: TImage
-    Left = 320
-    Top = 28
+    Left = 8
+    Top = 39
     Width = 249
     Height = 113
   end
   object DrawDiceBtn: TButton
-    Left = 80
-    Top = 72
-    Width = 185
+    Left = 8
+    Top = 8
+    Width = 249
     Height = 25
     Caption = 'Draw pair of random dice'
     TabOrder = 0
--- 01. Puzzle & Projects/Beginner/23 Beginner's Programs/Source/DrawDiceSource/U_Drawdice.pas
+++ 01. Puzzle & Projects/Beginner/23 Beginner's Programs/Source/DrawDiceSource/U_Drawdice.pas
@@ -3,15 +3,21 @@ unit U_Drawdice;
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, ExtCtrls;
+  SysUtils,
+  Classes,
+  Types,
+
+  Graphics,
+  Controls,
+  Forms,
+  StdCtrls,
+  ExtCtrls;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
     DrawDiceBtn: TButton;
     procedure DrawDiceBtnClick(Sender: TObject);
-    procedure FormActivate(Sender: TObject);
   end;
 
 var
@@ -21,94 +27,98 @@ implementation
 
 {$R *.DFM}
 
-{************ DrawDie *********}
-procedure drawdie(image:TImage; LeftTop:TPoint; diesize, n :integer);
-{Draw a single die at "lefttop" with side "diesize" and "n" dots}
+procedure DrawDie(image: TImage; LeftTop: TPoint; diesize, n: Integer);
 var
-  colwidth, rowheight:integer;
-  dotleft,dottop,dotsize:integer;
+  colwidth, rowheight: Integer;
+  dotleft, dottop, dotsize: Integer;
 
-    procedure drawdot(col,row:integer);
-    {local procedure to draw a single dot}
-    begin
-      image.canvas.ellipse(lefttop.x+col*colwidth+dotleft,
-                           lefttop.y+row*rowheight+dottop,
-                           lefttop.x+col*colwidth+dotleft+dotsize,
-                           lefttop.y+row*rowheight+dottop+dotsize);
-    end;
+  procedure DrawDot(col, row: Integer);
+  begin
+    image.Canvas.Ellipse(lefttop.x+col*colwidth+dotleft,
+                         lefttop.y+row*rowheight+dottop,
+                         lefttop.x+col*colwidth+dotleft+dotsize,
+                         lefttop.y+row*rowheight+dottop+dotsize);
+  end;
 
-begin  {drawdie}
-  with image do {this will let us automatically refer to properties and
-                 methods of image inside the following
-                 Begin ... End structure without writing image. each time}
+begin
+  with image do
   begin
     {clear the image and draw the outine}
-    canvas.brush.color:=clYellow;
-    canvas.Pen.color:=clBlack;
-    canvas.rectangle(rect(lefttop.x, lefttop.y, lefttop.x+diesize,lefttop.y+diesize));
-    canvas.brush.color:=clblack; {set brush to dot color}
+    Canvas.Brush.Color := clYellow;
+    Canvas.Pen.Color := clBlack;
+    Canvas.Rectangle(Rect(lefttop.x, lefttop.y, lefttop.x+diesize,lefttop.y+diesize));
+    Canvas.Brush.Color := clblack; {set brush to dot color}
+
     {to draw the dots we will divide the die face into 3 rows and 3 columns and
      put dots in the proper rows and colums for each number}
-    colwidth:=diesize div 3;  {column width, 1/3 of image width}
-    rowheight:=diesize div 3; {row height, 1/3 of image height}
-    dotleft:=colwidth div 4; {we'll put the left side of the dot 1/4 of the way
-                              across the column}
-    dottop:=rowheight div 4; {and the dot top 1/4 of the way down from the row top}
-    dotsize:=colwidth div 2; {make the size of the dot equal 1/2 the column width}
-    case n of      {draw a specific number by calling a "drawdot" procedure with
-                    the column and row number for each dot in the number.
-                    rows and columns are assumed to be numbered 0,1,2}
-      1: drawdot(1,1); {draw a simgle dot in the middle column and row}
-      2: begin         {draw 2 dots, top left and bottom righht}
-           drawdot(0,0);
-           drawdot(2,2);
-         end;
-      3: begin       {etc.}
-           drawdot(0,0);
-           drawdot(1,1);
-           drawdot(2,2);
-         end;
-      4: begin
-           drawdot(0,0);
-           drawdot(0,2);
-           drawdot(2,0);
-           drawdot(2,2);
-         end;
-      5: begin
-           drawdot(0,0);
-           drawdot(0,2);
-           drawdot(1,1);
-           drawdot(2,0);
-           drawdot(2,2);
-         end;
-      6: begin
-           drawdot(0,0);
-           drawdot(0,1);
-           drawdot(0,2);
-           drawdot(2,0);
-           drawdot(2,1);
-           drawdot(2,2);
-         end;
-    end; {case}
+    colwidth := diesize div 3;  {column width, 1/3 of image width}
+    rowheight := diesize div 3; {row height, 1/3 of image height}
+    dotleft := colwidth div 4;  {we'll put the left side of the dot 1/4 of the way across the column}
+    dottop := rowheight div 4; {and the dot top 1/4 of the way down from the row top}
+    dotsize := colwidth div 2; {make the size of the dot equal 1/2 the column width}
+
+    case n of
+    1:
+      begin
+        {draw a simgle dot in the middle column and row}
+        DrawDot(1, 1);
+      end;
+    2:
+      begin
+        {draw 2 dots, top left and bottom righht}
+        DrawDot(0, 0);
+        DrawDot(2, 2);
+      end;
+    3:
+      begin
+        {etc.}
+        DrawDot(0, 0);
+        DrawDot(1, 1);
+        DrawDot(2, 2);
+      end;
+    4:
+      begin
+        DrawDot(0, 0);
+        DrawDot(0, 2);
+        DrawDot(2, 0);
+        DrawDot(2, 2);
+      end;
+    5:
+      begin
+        DrawDot(0, 0);
+        DrawDot(0, 2);
+        DrawDot(1, 1);
+        DrawDot(2, 0);
+        DrawDot(2, 2);
+      end;
+    6:
+      begin
+        DrawDot(0, 0);
+        DrawDot(0, 1);
+        DrawDot(0, 2);
+        DrawDot(2, 0);
+        DrawDot(2, 1);
+        DrawDot(2, 2);
+      end;
+    end;
   end;
 end;
 
-{*********** DrawDiceBtnClick *********}
 procedure TForm1.DrawDiceBtnClick(Sender: TObject);
-var size, offset:integer;
+var
+  size, offset: Integer;
 begin
-  image1.canvas.brush.color:=color;
-  with image1 do canvas.fillrect(clientrect);
-  size:=9*image1.height div 10; {make size 90% of height}
-  offset:=image1.height div 20; {make offset 5% of height}
-  drawdie(image1, point(offset,offset),size,random(6)+1); {draw 1st die}
-  drawdie(image1, point(offset+image1.width div 2,offset),size,random(6)+1); {draw 2nd}
+  image1.Canvas.Brush.Color := color;
+  with image1 do
+    Canvas.FillRect(ClientRect);
+
+  size := 9*image1.height div 10; {make size 90% of height}
+  offset := image1.height div 20; {make offset 5% of height}
+
+  DrawDie(image1, Point(offset, offset), size, random(6)+1); {draw 1st die}
+  DrawDie(image1, Point(offset+image1.width div 2, offset), size, random(6)+1); {draw 2nd}
 end;
 
-procedure TForm1.FormActivate(Sender: TObject);
-begin
-  RANDOMIZE;  {start with a random seed so results do not repeat from run ro run}
-              {for testing it is often a good idea not to set the random seed}
-end;
-
+initialization
+  Randomize;
 end.
